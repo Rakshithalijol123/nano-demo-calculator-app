e82e6350472a38b00f66df375e7ba926eb5f071a
Rakshit Halijol <20beam031@dsce.edu.in>
2023-08-06 18:35:46 +0530

    Added given endpoints

--- server.py
+++ server.py
@@ -1,19 +1,47 @@
-from flask import Flask
+# from flask import Flask
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
 
-@app.route("/calculator/greeting", methods=['GET'])
+# @app.route("/calculator/greeting", methods=['GET'])
+# def greeting():
+#     return ''
+
+# @app.route("/calculator/add", methods=['POST'])
+# def add():
+#     return ''
+
+# @app.route("/calculator/subtract", methods=['POST'])
+# def subtract():
+#     return ''
+
+# if __name__ == '__main__':
+#     app.run(port=8080,host='0.0.0.0')
+
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    first: int
+    second: int
+
+
+@app.get("/calculator/greeting")
 def greeting():
-    return ''
+    return {"result": "Hello world!"}
 
-@app.route("/calculator/add", methods=['POST'])
-def add():
-    return ''
 
-@app.route("/calculator/subtract", methods=['POST'])
-def subtract():
-    return ''
+@app.post("/calculator/add")
+def add(item: Item):
+    result = item.first + item.second
+    return {"result": result}
 
-if __name__ == '__main__':
-    app.run(port=8080,host='0.0.0.0')
+
+@app.post("/calculator/subtract")
+def subtract(item: Item):
+    result = item.first - item.second
+    return {"result": result}
