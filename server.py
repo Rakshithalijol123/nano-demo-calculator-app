e105fce4190cbd68f7161efc405ba755913e3e50
Rakshit Halijol <20beam031@dsce.edu.in>
2023-08-06 18:52:51 +0530

    Added given endpoints

--- server.py
+++ server.py
@@ -1,26 +1,27 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from flask import Flask, request, jsonify
 
-app = FastAPI()
-
-
-class Item(BaseModel):
-    first: int
-    second: int
+app = Flask(__name__)
 
 
-@app.get("/calculator/greeting")
+@app.route("/calculator/greeting", methods=['GET'])
 def greeting():
     return "Hello world!"
 
 
-@app.post("/calculator/add")
-def add(item: Item):
-    result = item.first + item.second
-    return {"result": result}
+@app.route("/calculator/add", methods=['POST'])
+def add():
+    data = request.get_json()
+    first = data["first"]
+    second = data['second']
+    return jsonify({"result": first+second})
 
 
-@app.post("/calculator/subtract")
-def subtract(item: Item):
-    result = item.first - item.second
-    return {"result": result}
+@app.route("/calculator/subtract", methods=['POST'])
+def subtract():
+    data = request.get_json()
+    first = data["first"]
+    second = data['second']
+    return jsonify({"result": first-second})
+
+if __name__ == '__main__':
+    app.run(port=8080,host='0.0.0.0')
